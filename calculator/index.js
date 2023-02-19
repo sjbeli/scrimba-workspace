76ff0799a06931bb3fff4c0d8b00a286a3243561
Steve <steven.beliveau2@gmail.com>
2023-02-18 22:10:16 -0500

    load calculator

--- calculator/index.js
+++ calculator/index.js
@@ -1,5 +1,5 @@
-let num1 = 8
-let num2 = 2
+let num1 = 10
+let num2 = 5
 document.getElementById("num1-el").textContent = num1
 document.getElementById("num2-el").textContent = num2
 
@@ -10,5 +10,25 @@ document.getElementById("num2-el").textContent = num2
 
 // E.g. if the user clicks on the "Plus" button, you should render
 // "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+let sumEl = document.getElementById("sum-el")
 
+function add() {
+    let result = num1 + num2
+    sumEl.textContent = "Sum: " + result
+}
+
+function subtract() {
+    let result = num1 - num2
+    sumEl.textContent = "Sum: " + result
+}
+
+function divide() {
+    let result = num1 / num2
+    sumEl.textContent = "Sum: " + result
+}
+
+function multiply() {
+    let result = num1 * num2
+    sumEl.textContent = "Sum: " + result
+}
 
