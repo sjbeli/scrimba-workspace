fd509ce04b467fbbd7b94d737eb5f7a2591aa68e
Steve <steven.beliveau2@gmail.com>
2023-02-23 01:11:56 -0500

    update scrimba workspace - js-toolin

--- js-toolin/index.js
+++ js-toolin/index.js
@@ -1,32 +1,97 @@
-// let course = {
-//     title: "Learn CSS Grid for free",
-//     lessons: 16,
-//     creator: "Per Harald Borgen",
-//     length: 63,
-//     level: 2,
-//     isFree: true,
-//     tags: ["html", "css"]
+// Create a person object that contains three keys: name, age, and county.
+// Use yourself as an example to set the values for name, age, and country
+
+// Create a function, logData(), that uses the person object to create a
+// string in the following format: 
+// "Per is 35 years old and lives in Norway"
+
+// Call the logData() function to verify that it works
+
+// let person = {
+//     pName: "Steve",
+//     age: 55,
+//     country: "USA"
 // }
 
+// function logData() {
+//     console.log(person.pName + " is " + person.age + " years old and lives in " + person.country  )
+// }
 
-// console.log( course.tags)
+// logData()
+// --------------------------
 
 
-// Create an object that represents an airbnb castle listing.
-// It should contain at least one boolean, one string, one number, and one array
-// Log out at least two of the keys using the dot notation
+// let age = 27
+
+// less than 6 years old -> free
+// 6 to 17 years old     -> child discount
+// 18 to 26 years old    -> student discount
+// 27 to 66 years old    -> full price
+// over 66 years old     -> senior citizen discount
+
+// Create a conditional statement (if/else/else if) that logs out the discount
+// the passenger will get based upon the value of the age variable
+
+// if ( age < 6 ) {
+//     console.log('discount = free')
+// } else if ( age < 18 ) {
+//     console.log('child discount')
+// } else if ( age < 27 ) {
+//     console.log('student discount')
+// } else if ( age < 67 ) {
+//     console.log('full price, no discount available')
+// } else {
+//     console.log('senior citizen discount')
+// }
+// --------------------------
+
+// let largeCountries = ["China","India","USA","Indonesia","Pakistan"]
+
+/* Use a for loop to log the following to the console:
+
+The 5 largest countries in the world:
+- China
+- India
+- United States
+- Indinesia
+- Pakistan
+*/
+
+// console.log("The 5 largest countries is the world are:")
+// for ( let i = 0; i < largeCountries.length; i++ ) {
+//     console.log('- ' + largeCountries[i])
+// }
+// --------------------------
+
+// let largeCountries = ["Tuvalu","India","USA","Indonesia","Monaco"]
+
+// You need to help me fixup the largeCountries array so that 
+// China and Pakistan are added back into their respective places
+
+// Use push() & pop() and their counterparts unshift() & shift()
+// Google how to use unshift() and shift()
+
+// largeCountries.pop()
+// largeCountries.push("Pakistan")
+// largeCountries.shift()
+// largeCountries.unshift("China")
+
+// console.log(largeCountries)
+// --------------------------
+
+// let dayOfMonth = 13
+// let weekday = "Friday"
+
+// If it is Friday the 13th, log out this spooky face: 😱
+// Use the logical "AND operator" -> &&
+
+// if ( dayOfMonth === 13 && weekday === "Friday" ) {
+//     console.log('😱')
+// }
+// --------------------------
+
+let hands = ["rock", "paper", "scissor"]
+
+// Create a function that returns a random item from the array
 
 
-let castle = {
-    title: "Live in my castle today",
-    isAvailable: true,
-    address: "123 Castle Way",
-    price: 200,
-    location: [
-        "Paris", 
-        "Yorkshire",
-        "Heaven"
-    ]
-}
-
-console.log( castle.isAvailable + " " + castle.location )
